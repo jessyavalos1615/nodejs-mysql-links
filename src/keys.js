a0a251c0263f668ea12a1ffc2a4b74a13b52b105
Jessy Avalos Barrales <jessyavalos16@outlook.es>
2019-07-09 11:10:51 -0500

    First commit

--- src/keys.js
+++ src/keys.js
@@ -4,7 +4,7 @@ module.exports = {
         connectionLimit: 10,
         host: 'localhost',
         user: 'root',
-        password: 'Password1',
+        password: '',
         database: 'db_links'
     }
 
